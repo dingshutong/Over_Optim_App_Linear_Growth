70a982a671c9bacfd6f4d29dd9f935c1cc8b7bc0
Shutong <shutong.ding@gmail.com>
2015-06-11 12:31:52 +0200

    fix the pmp for the subdimension models

--- lin_gm_top5.R
+++ lin_gm_top5.R
@@ -26,6 +26,22 @@ dec2bin <- function(x,k){
 
 dec2bin_vec =  Vectorize(dec2bin,"x")
 
+combn2model = function(kmax,sub_dim){
+  ax = combn(kmax,sub_dim)
+  num_mod  = ncol(ax)
+  models = matrix(0,num_mod,kmax)
+  gix = function(axi,kmax){
+      gi = numeric(kmax)
+      gi[axi] = 1
+      return(gi)
+  }
+  
+  models = t(apply(ax,2,gix,kmax))
+  return(models)
+  
+}
+
+
 
 # the log marginal likelihood based on Fernandez, Ley and Steel(2001)
 # p. 566 (8)
@@ -75,7 +91,7 @@ log_mml_gprior_vec = Vectorize(log_mml_gprior, "midx" )
 
 # compute the posterior model probabilites based on all combination of models
 # using kmax number of variables, 2^kmax in total 
-pmp_all_modes = function(boot_vec, xymat, kmax , n, keep_cons = FALSE){
+pmp_all_models = function(boot_vec, xymat, kmax , n, keep_cons = FALSE){
   
   # make all the permutations for the model, models
   if ( keep_cons ) {
@@ -118,11 +134,56 @@ bootstrap_pmp_all = function(bootrep, xymat, kmax, keep_cons = FALSE) {
      sample(x,length(x),replace=TRUE)
    }
   boot_seeds =  apply(boot_seeds,1,gen_x)
-  pmp_all = t(apply(boot_seeds, 2, pmp_all_modes, xymat, kmax, n, keep_cons))
+  pmp_all = t(apply(boot_seeds, 2, pmp_all_models, xymat, kmax, n, keep_cons))
   
   return(list("pmp"=pmp_all, "model"=models))
   
 } 
+
+pmp_sub_models = function(boot_vec, models, xymat, kmax , n, keep_cons = FALSE){
+  
+  # convert models indexes into indx
+  if ( keep_cons ) {
+    mod_indx = c(0,apply(models,1,bin2dec))
+  }
+  else {
+    mod_indx = apply(models,1,bin2dec)
+  }
+  
+  lml_all = log_mml_gprior_vec(xymat[boot_vec,], mod_indx , kmax, n)
+  lml_max = max(lml_all)
+  lml_all = exp(lml_all - lml_max)
+  c_lml   =  sum(lml_all)   
+  return(lml_all/c_lml)
+  
+}
+
+
+
+bootstrap_pmp_sub = function(bootrep, models, xymat, kmax, keep_cons = FALSE) {
+  
+  n = nrow(xymat)
+  boot_seeds = t(matrix(rep(1:n,bootrep), n, bootrep))
+  
+  gen_x = function(x){
+    sample(x,length(x),replace=TRUE)
+  }
+  boot_seeds =  apply(boot_seeds,1,gen_x)
+  pmp_sub = t(apply(boot_seeds, 2, pmp_sub_models, models, xymat, kmax, n, keep_cons))
+  
+  return(list("pmp"=pmp_sub, "model"=models))
+  
+} 
+
+
+
+
+
+
+
+
+
+
 
 
 #================== Local Functions End ===================
@@ -131,7 +192,7 @@ bootstrap_pmp_all = function(bootrep, xymat, kmax, keep_cons = FALSE) {
 #--------------- Begin the INPUT --------------
 
 # load the data file 
-data_dir = "C:/Users/shudi32/Desktop"
+data_dir = "C:/Users/shudi32/Dropbox/OverOptim/SD_part/Applications/Linear regression_Growth model/R top 5 model"
 setwd(data_dir)
 datmat   = read.csv(file="gm_steel_xymat.csv",header=T,sep=";")
 kmax     = 5
@@ -139,24 +200,41 @@ xymat    = as.matrix(datmat[,1:(kmax+1)])
 # 
 n        = nrow(xymat)
 # number of bootstrap replicates 
-bootrep  = 10000
+bootrep  = 100
 # whether to keep the constant model in comparsion 
 keep_cons= FALSE
-
+# check sub-dimension models 
+sub_dim  = 3
 #--------------- End  the INPUT --------------- 
+
+# -------------------Set up -------------------
+# generate the model indexes, binary
+models = combn2model(kmax,sub_dim)
 
 
 
 
 # -------------- excution example ---------------
-log_mml_gprior(xymat, 1, kmax, n)
-log_mml_gprior(xymat, 2, kmax, n)
-log_mml_gprior(xymat, 3, kmax, n)
+#log_mml_gprior(xymat, 1, kmax, n)
+#log_mml_gprior(xymat, 2, kmax, n)
+#log_mml_gprior(xymat, 3, kmax, n)
+#log_mml_gprior_vec(xymat,1:3,kmax,n)
+#pmp_all_models(1:n, xymat, kmax, n, keep_cons = FALSE )
 
-log_mml_gprior_vec(xymat,1:3,kmax,n)
-pmp_all_modes(1:n, xymat, kmax, n, keep_cons = FALSE )
+
+pmp_boot_sub3 = bootstrap_pmp_sub(bootrep, models, xymat, kmax, keep_cons)
 
 pmp_boot = bootstrap_pmp_all(bootrep, xymat, kmax, keep_cons)
 write.table(file="pmp_all_boot.txt",pmp_boot$pmp, row.names = FALSE, col.names=FALSE)
 write.table(file="pmp_all_models.txt",pmp_boot$model, row.names = FALSE, col.names=FALSE)
+
+
+# make some plots 
+require(coda)
+pmp_sub3 = as.mcmc(pmp_boot_sub3$pmp) 
+matplot(pmp_sub3,type="l")
+
+
+
+
 
